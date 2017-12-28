ea866cf6be07455eb5f4cefe25e7c49a2741071a
Derek Zeller <Derek.Zeller@nelnet.net>
2017-12-28 11:00:26 -0600

    added consume REST tutorial

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,13 +1,27 @@
 package hello;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+//https://spring.io/guides/gs/serving-web-content/
+    //public static void main(String[] args) {
+    //    SpringApplication.run(Application.class, args);
+    //}
+
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+// https://spring.io/guides/gs/consuming-rest/
+    public static void main(String args[]) {
+        RestTemplate restTemplate = new RestTemplate();
+        Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+        log.info(quote.toString());
     }
+
 
 }
